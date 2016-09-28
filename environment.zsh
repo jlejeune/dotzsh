8eb5e4e1e50fadc9f9c06009b0aaebeb4c82ca42
jlejeune <mail.julien.le.jeune@gmail.com>
2016-09-28 21:29:54 +0200

    Add pip support

--- environment.zsh
+++ environment.zsh
@@ -2,3 +2,6 @@ export LC_ALL=fr_FR.UTF-8
 export LANG=fr_FR.UTF-8
 export LANGUAGE=fr_FR.UTF-8
 export EDITOR=/usr/bin/vim
+export CLICOLOR=1
+# pip should only run if there is a virtualenv currently activated
+export PIP_REQUIRE_VIRTUALENV=true
