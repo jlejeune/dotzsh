ca72c9a587a1013479257ac74fa6887711385d0f
Julien Le Jeune <julien.le-jeune@corp.ovh.com>
2017-12-13 14:18:35 +0100

    Add docker aliases

--- functions.zsh
+++ functions.zsh
@@ -2,13 +2,3 @@
 gpip() {
     PIP_REQUIRE_VIRTUALENV="" pip "$@"
 }
-
-# Remove docker containers
-drm() {
-    docker rm $(docker ps -q -a);
-}
-
-# Remove docker images
-dri() {
-    docker rmi $(docker images -q);
-}
