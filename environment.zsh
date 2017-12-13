0d51861238433a79455a87f1c97a717a812dfc9f
Julien Le Jeune <julien.le-jeune@corp.ovh.com>
2017-12-13 14:09:19 +0100

    Update $PATH

--- environment.zsh
+++ environment.zsh
@@ -5,3 +5,4 @@ export EDITOR=/usr/bin/vim
 export CLICOLOR=1
 # pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=true
+export PATH="$HOME/bin:$HOME/dev/scripts:$HOME/dev/tools:$PATH"
