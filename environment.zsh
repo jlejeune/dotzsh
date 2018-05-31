6c0fdb621b36b26a2f80a4ebe81c072fa396cdf8
Julien LE JEUNE <mail.julien.le.jeune@gmail.com>
2018-05-31 17:47:40 +0200

    Update environment.zsh
    Add miniconda binaries paths in PATH

--- environment.zsh
+++ environment.zsh
@@ -5,4 +5,4 @@ export EDITOR=/usr/bin/vim
 export CLICOLOR=1
 # pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=true
-export PATH="$HOME/bin:$HOME/dev/scripts:$HOME/dev/tools:$PATH"
+export PATH="$HOME/bin:$HOME/dev/scripts:$HOME/dev/tools:$HOME/miniconda2/bin:$HOME/miniconda3/bin:$PATH"
